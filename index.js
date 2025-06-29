caa93265fb156d9c9820e03d73ca85ae842af51f
Amayes2126 <berradjamayes@gmail.com>
2025-06-29 22:54:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.post('/', (req, res) => {
   lastData = req.body;
   console.log('Données reçues:', lastData);
+  buzzerMuted = false; // Remet à zéro après chaque nouvelle mesure
   res.send({ status: 'OK', message: 'Données reçues' });
 });
 
